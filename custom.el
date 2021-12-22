75c74a689ca81f24400711030a7e4b8dd512d496
madand <dev@madand.net>
2021-12-22 21:32:26 +0200

    Add several new packages

--- custom.el
+++ custom.el
@@ -4,10 +4,25 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-blinks 3)
+ '(history-delete-duplicates t)
+ '(history-length 1000)
+ '(modus-themes-bold-constructs t)
+ '(modus-themes-inhibit-reload nil)
+ '(modus-themes-italic-constructs t)
  '(org-src-window-setup 'current-window)
  '(safe-local-variable-values
    '((common-lisp-style . modern)
      (comment-style . multi-line)
      (toc-org-max-depth . 4)
      (eval add-hook 'after-save-hook #'my-compile-init-file 91 t)
-     (eval add-hook 'after-save-hook #'org-babel-tangle 90 t))))
+     (eval add-hook 'after-save-hook #'org-babel-tangle 90 t)))
+ '(use-dialog-box t)
+ '(use-short-answers t)
+ '(visible-bell t)
+ '(x-select-enable-clipboard-manager nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
